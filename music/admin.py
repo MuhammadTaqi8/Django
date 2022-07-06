2c1aecdbe5abdb2d0c1aea600be676d0dfcfe64b
MuhammadTaqi8 <taqi0413@gmail.com>
2022-07-06 13:20:28 +0500

    added data models to music app

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,6 @@
+import imp
 from django.contrib import admin
+from music.models import Album
 
 # Register your models here.
+admin.site.register(Album)
--- music/models.py
+++ music/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    albumTitle = models.CharField(max_length=250)
+    genre = models.CharField(max_length=250)
+    albumLogo = models.CharField(max_length=250)
+    def __str__(self):
+        return self.albumTitle + ' - ' + self.artist
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE) 
+    fileType = models.CharField(max_length=250)
+    songTitle = models.CharField(max_length=250)
